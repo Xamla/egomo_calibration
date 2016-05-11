9a133dfcafa4be33361ac697a1d47f91f1bf9350
Andreas Köpf <andreas.koepf@xamla.com>
2016-05-11 17:09:30 +0200

    Ues dynamic calibration date in demo

--- examples/multi_capture.lua
+++ examples/multi_capture.lua
@@ -3,7 +3,7 @@ local calib = require 'egomo_calibration'
 local xamla3d = calib.xamla3d
 
 
-local output_directory_path = '/data/ur5/ur5_calibration/2016-05-11'
+local output_directory_path = '/data/ur5/ur5_calibration/' .. os.date('%Y-%m-%d') .. '/'
 local pictures_per_position = 30
 local velocity_scaling = 0.5
 
