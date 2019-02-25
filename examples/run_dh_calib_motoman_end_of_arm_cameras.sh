6c0d9a9195f9830c08e5346324ef7377098f8701
inga <inga.altrogge@xamla.com>
2019-02-25 09:30:52 +0100

    changed run-file

--- examples/run_dh_calib_motoman_end_of_arm_cameras.sh
+++ examples/run_dh_calib_motoman_end_of_arm_cameras.sh
@@ -16,7 +16,30 @@
 
 # Joint offset optimization for theta:
 # ====================================
-python3 dh_calib_motoman_end_of_arm_cameras.py "../../left_arm_onboard_new/2019-01-17_50Images_forCalib/stereo_cams_4103130811_4103189394.npy" "../../left_arm_onboard_new/2019-01-17_50Images_forCalib/FlangeEye.npy" 50 "../../left_arm_onboard_new/2019-01-17_50Images_forCalib/capture_all/cam_4103130811" "../../left_arm_onboard_new/2019-01-17_50Images_forCalib/capture_all/cam_4103189394" "../../left_arm_onboard_new/2019-01-17_50Images_forCalib/jsposes_tensors.npy" "../../left_arm_onboard_new/2019-01-17_50Images_forCalib/all_vals_tensors.npy" 22 "results_leftArm_leftArmCams/" "robotModel_theta_optimized_leftArm_withLeftArmCams" "handEye_theta_optimized_leftArm_withLeftArmCams" "left"
+#python3 dh_calib_motoman_end_of_arm_cameras.py \
+#"../data/left_arm/2019-01-24/stereo_cams_4103130811_4103189394.npy" \
+#"../data/left_arm/2019-01-24/HandEye.npy" \
+#55 \
+#"../data/left_arm/2019-01-24/capture_all/cam_4103130811" \
+#"../data/left_arm/2019-01-24/capture_all/cam_4103189394" \
+#"../data/left_arm/2019-01-24/jsposes_tensors.npy" \
+#"../data/left_arm/2019-01-24/all_vals_tensors.npy" \
+#22 \
+#"results_left_arm/" \
+#"robotModel_leftArm_2019-01-24" \
+#"handEye_leftArm_2019-01-24" \
+#"left"
 
-#python3 dh_calib_motoman_end_of_arm_cameras.py "../../right_arm_onboard/2019-01-16_50Images_forCalib/stereo_cams_4103217455_4103235743_2019-01-16_111536_sphere50.npy" "../../right_arm_onboard/2019-01-16_50Images_forCalib/FlangeEye_initialGues_2019-01-14_125243_sphere14.npy" 50 "../../right_arm_onboard/2019-01-16_50Images_forCalib/capture_all/cam_4103217455" "../../right_arm_onboard/2019-01-16_50Images_forCalib/capture_all/cam_4103235743" "../../right_arm_onboard/2019-01-16_50Images_forCalib/jsposes_tensors.npy" "../../right_arm_onboard/2019-01-16_50Images_forCalib/all_vals_tensors.npy" 22 "results_rightArm_rightArmCams/" "robotModel_theta_optimized_rightArm_withRightArmCams" "handEye_theta_optimized_rightArm_withRightArmCams" "right"
-
+python3 dh_calib_motoman_end_of_arm_cameras.py \
+"../data/right_arm/2019-02-05/stereo_cams_4103217455_4103235743.npy" \
+"../data/right_arm/2019-02-05/HandEye.npy" \
+55 \
+"../data/right_arm/2019-02-05/capture_all/cam_4103217455" \
+"../data/right_arm/2019-02-05/capture_all/cam_4103235743" \
+"../data/right_arm/2019-02-05/jsposes_tensors.npy" \
+"../data/right_arm/2019-02-05/all_vals_tensors.npy" \
+22 \
+"results_right_arm/" \
+"robotModel_rightArm_2019-02-05" \
+"handEye_rightArm_2019-02-05" \
+"right"
