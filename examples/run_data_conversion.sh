4fab2866dfd56475c5689fdee1071c6130bf4f58
inga <inga.altrogge@xamla.com>
2019-02-27 12:48:10 +0100

    generalized run-file for data conversion. To call the generalized run-file, go into the data folder and call the run-file from there (e.g. ../../../examples/run_data_conversion.sh)

--- examples/run_data_conversion.sh
+++ examples/run_data_conversion.sh
@@ -5,20 +5,23 @@
 # calibration path of output pythonfile
 # hand-eye path of input torchfile
 # hand-eye path of output pythonfile
-# jsposes path of input torchfile
-# jsposes path of output pythonfile
-# all_joint_values path of intput torchfile
-# all_joint_values path of output pythonfile
+# jsposes_tensors path of input torchfile
+# jsposes_tensors path of output pythonfile
+# all_vals_tensors path of intput torchfile
+# all_vals_tensors path of output pythonfile
 
-#python3 data_conversion.py "../../calibration_rand50/stereo_cams_4103130811_4103189394.t7" "../../calibration_rand50/stereo_cams_4103130811_4103189394.npy" "../../calibration_rand50/HandEye.t7" "../../calibration_rand50/HandEye.npy" "../../calibration_rand50/jsposes_tensors.t7" "../../calibration_rand50/jsposes_tensors.npy" "../../calibration_rand50/all_vals_tensors.t7" "../../calibration_rand50/all_vals_tensors.npy"
+# Don't forget to adapt stereo camera IDs!
 
-#python3 data_conversion.py "../../calib_after_all/stereo_cams_4103130811_4103189394_new.t7" "../../calib_after_all/stereo_cams_4103130811_4103189394_new.npy" 'None' 'None' "../../calib_after_all/jsposes_tensors.t7" "../../calib_after_all/jsposes_tensors.npy" "../../calib_after_all/all_vals_tensors.t7" "../../calib_after_all/all_vals_tensors.npy"
+mydir="$(dirname "$0")"
+echo $mydir
 
-#python3 data_conversion.py "../../calib_before_all/stereo_cams_4103130811_4103189394_new.t7" "../../calib_before_all/stereo_cams_4103130811_4103189394_new.npy" 'None' 'None' "../../calib_before_all/jsposes_tensors.t7" "../../calib_before_all/jsposes_tensors.npy" "../../calib_before_all/all_vals_tensors.t7" "../../calib_before_all/all_vals_tensors.npy"
-
-#python3 data_conversion.py "../../right_arm_data/calib_robot_at_BASF/stereo_cams_CAMAU1639044_CAMAU1639035.t7" "../../right_arm_data/calib_robot_at_BASF/stereo_cams_CAMAU1639044_CAMAU1639035.npy" "../../right_arm_data/calib_robot_at_BASF/HandPattern.t7" "../../right_arm_data/calib_robot_at_BASF/HandPattern.npy" "../../right_arm_data/calib_robot_at_BASF/jsposes_tensors.t7" "../../right_arm_data/calib_robot_at_BASF/jsposes_tensors.npy" 'None' 'None'
-
-python3 data_conversion.py "None" "None" "None" "None" "../../right_arm_data/calib_after_27_21/jsposes_tensors_new.t7" "../../right_arm_data/calib_after_27_21/jsposes_tensors_new.npy" "../../right_arm_data/calib_after_27_21/all_vals_tensors_21.t7" "../../right_arm_data/calib_after_27_21/all_vals_tensors_21.npy"
-
-
+python3 $mydir/data_conversion.py \
+"stereo_cams_4103217455_4103235743.t7" \
+"stereo_cams_4103217455_4103235743.npy" \
+"HandEye.t7" \
+"HandEye.npy" \
+"jsposes_tensors.t7" \
+"jsposes_tensors.npy" \
+"all_vals_tensors.t7" \
+"all_vals_tensors.npy"
 
